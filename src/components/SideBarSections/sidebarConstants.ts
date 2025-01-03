d454dedab8622768eb42cce0e004cee248726008
Chris Casey <cdcasey@users.noreply.github.com>
2025-01-02 20:42:15 -0600

    Images radomized and formatted

---
+++ src/components/SideBarSections/sidebarConstants.ts
@@ -0,0 +1,45 @@
+import Image1 from '@/assets/sidebar/01.jpg';
+import Image2 from '@/assets/sidebar/02.jpg';
+import Image3 from '@/assets/sidebar/03.jpg';
+import Image4 from '@/assets/sidebar/04.jpg';
+import Image5 from '@/assets/sidebar/05.jpg';
+import Image6 from '@/assets/sidebar/06.jpg';
+import Image7 from '@/assets/sidebar/07.jpg';
+
+export const IMAGE_INFO = [
+	{
+		image: Image1,
+		alt: 'Chris with headphones on getting ready to record',
+		caption: 'Chris getting ready to record.',
+	},
+	{
+		image: Image2,
+		alt: 'Hallie in a greenhouse while smiling at the camera.',
+		caption: 'Hallie in a greenhouse.',
+	},
+	{
+		image: Image3,
+		alt: 'Hallie, Chris, and Catherine smiling at the camera and celebrating one year of podcasting',
+		caption: 'Hallie, Chris, and Catherine celebrating one year of podcasting!',
+	},
+	{
+		image: Image4,
+		alt: 'A man and a woman smiling at the camera while standing in a greenhouse',
+		caption: 'Chris visiting Hallie at the greenhouse at Texas State.',
+	},
+	{
+		image: Image5,
+		alt: 'Chris and Hallie smiling at the camera while on a hike',
+		caption: 'Chris and Hallie on a hike.',
+	},
+	{
+		image: Image6,
+		alt: 'A woman smiling with her arms spread out wide while standing in front of a corn field',
+		caption: 'Hallie in front of her favorite tall grass.',
+	},
+	{
+		image: Image7,
+		alt: 'Hallie and Chris smiling at the camera while at lunch together',
+		caption: 'Hallie and Chris at lunch together.',
+	},
+];
