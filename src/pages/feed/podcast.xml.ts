3e0fdb128037056cab79d666662f8be941368d20
Chris Casey <cdcasey@users.noreply.github.com>
2025-12-17 18:54:55 -0600

    Adding feed plus sample feed file

---
+++ src/pages/feed/podcast.xml.ts
@@ -0,0 +1,59 @@
+import rss from "@astrojs/rss";
+import type { APIContext } from "astro";
+import { getCollection } from "astro:content";
+export async function GET(context: APIContext) {
+	const posts = await getCollection("posts");
+	// Filter to episodes
+	const episodes = posts
+		.filter(
+			(post) =>
+				post.data.categories.includes("episodes") &&
+				post.data.audio &&
+				!post.data.slug.endsWith("-transcript"),
+		)
+		.sort((a, b) => b.data.date.getTime() - a.data.date.getTime());
+	return rss({
+		title: "One to Grow On",
+		description: "Understanding how food production impacts us and our world",
+		site: context.site!,
+		xmlns: {
+			itunes: "http://www.itunes.com/dtds/podcast-1.0.dtd",
+			podcast: "https://github.com/Podcastindex-org/podcast-namespace/blob/main/docs/1.0.md",
+			content: "http://purl.org/rss/1.0/modules/content/",
+		},
+		// Copy your extensive channel metadata from rss.xml here
+		customData: `
+      <language>en-US</language>
+      <itunes:author>Hallie Casey, Chris Casey</itunes:author>
+      <itunes:type>episodic</itunes:type>
+      <itunes:owner>
+        <itunes:name>Hallie Casey, Chris Casey</itunes:name>
+        <itunes:email>onetogrowonpod@gmail.com</itunes:email>
+      </itunes:owner>
+      <itunes:image href="https://12go.onetogrowonpod.com/wp-content/uploads/2019/04/OTGO-1400.jpg" />
+      <itunes:category text="Science">
+        <itunes:category text="Nature" />
+      </itunes:category>
+      <!-- ... other categories -->
+    `,
+		items: episodes.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.date,
+			description: post.data.excerpt,
+			link: `/${post.data.slug}/`,
+			enclosure: {
+				url: post.data.audio!,
+				length: 0, // TODO: Add 'audioSize' to frontmatter to populate this
+				type: "audio/mpeg",
+			},
+			customData: `
+        <itunes:summary><![CDATA[${post.data.excerpt}]]></itunes:summary>
+        <itunes:explicit>clean</itunes:explicit>
+        <itunes:episodeType>full</itunes:episodeType>
+        <!-- TODO: Add 'duration' to frontmatter to populate this -->
+        <!-- <itunes:duration>${post.data.duration}</itunes:duration> -->
+        <content:encoded><![CDATA[${post.body}]]></content:encoded>
+      `,
+		})),
+	});
+}
